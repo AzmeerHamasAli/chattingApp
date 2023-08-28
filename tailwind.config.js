9d41eefc3ea311d715f62f9329a3fb36ad182715
AzmeerHamasAli <apnawqtaiga@gmail.com>
2023-08-28 17:14:32 +0500

    UI completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
    content: ["**/*.{html,js,jsx,ts,tsx}"],
    theme: {
-      extend: {},
+      extend: {
+         fontFamily: {
+            poppins: "Poppins",
+         },
+         colors: {
+            barColor: "#EFEFEF",
+            bodyColor: "#f5f5f5",
+         },
+      },
    },
    plugins: [],
 };
